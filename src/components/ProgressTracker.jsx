9bbd9701d35752f723bc1113fe9c2ed9ef6807da
smyler3 <mylertalym01@gmail.com>
2024-08-28 01:27:40 +1000

    feat: begin adding progress tracker

---
+++ src/components/ProgressTracker.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import '../styles/ProgressTracker.css'
+
+const StepMarker = ({ label, icon, isActive, isComplete }) => {
+    return (
+        <div className={`step-header ${isActive ? 'step-active' : ''} ${isComplete ? 'step-complete' : ''}`}>
+            <div className="step-icon">
+                {icon && <img src="icon" alt="" />}
+            </div>
+            <div className="step-label">{ label }</div>
+        </div>
+    );
+};
+
+const ProgressBar = ({ currentStep, totalSteps }) => {
+    const progress = (currentStep / totalSteps) * 100;
+
+    return (
+        <div className="progress-bar">
+            <div className="progress" style={{ width: `${progress}%` }} />
+        </div>
+    );
+};
+
+const ProgressTracker = ({ steps }) => {
+    const [currentStep, setCurrentStep] = useState(0);
+
+    // Render nothing if steps is empty
+    if (steps.length === 0) {
+        return null;
+    };
+
+    return (
+        <div className="progress-tracker">
+            <div className="step-indicators">
+                {steps.map((step, index) => (
+                    <StepMarker
+                        key={index}
+                        label={step.label}
+                        icon={step.icon}
+                        isActive={index === currentStep}
+                        isComplete={index < currentStep}
+                    />
+                ))}
+            </div>
+            <ProgressBar currentStep={currentStep} totalSteps={steps.length}/>
+        </div>
+    )
+};
+
+export default ProgressTracker;
