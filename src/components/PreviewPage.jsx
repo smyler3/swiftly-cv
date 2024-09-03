1026eef4d6440e047290903cd30684b744353c06
smyler3 <mylertalym01@gmail.com>
2024-09-04 00:57:56 +1000

    feat: begin adding PreviewPage component

---
+++ src/components/PreviewPage.jsx
@@ -0,0 +1,14 @@
+import NavigationButtons from './NavigationButtons';
+
+const PreviewPage = () => {
+    return (
+        <main>
+            <div className='content-page'>
+                <h2 className="form-title">PREVIEW</h2>
+            </div>
+            <NavigationButtons />
+        </main>
+    )
+}
+
+export default PreviewPage;
