9137d74eb86012188b954bb475c8b9e70ba47aed
smyler3 <mylertalym01@gmail.com>
2024-10-07 22:37:44 +1100

    feat: add preview page contact icons and defence mechanisms against empty inputs

--- src/components/PreviewPage.jsx
+++ src/components/PreviewPage.jsx
@@ -1,26 +1,58 @@
+import phoneIcon from "../assets/icons/phoneIcon.svg";
+import emailIcon from "../assets/icons/emailIcon.svg";
+import locationIcon from "../assets/icons/locationIcon.svg";
+import websiteIcon from "../assets/icons/websiteIcon.svg";
 import "../styles/PreviewPage.css";
 
-const ExperienceItem = (data) => {
+// Returns a date picker input date in mm/yyyy format
+function convertDate(date) {
+    const nums = date.split('-');
+    return (nums[1] + '/' + nums[0]);
+};
+
+// Checks if a data object has any properties which aren't an empty string
+function dataIsEmpty(data) {
     return (
+        Object.keys(data)
+            .filter(key => key !== 'sectionID') // Ignore sectionID as it will always have a value
+            .every(key => data[key] === '')
+    );
+}
+
+const ExperienceItem = ({ data }) => {
+    return !dataIsEmpty(data) ? (
         <div className="experience-item">
-            <p className="experience-company">{data.companyName}, {data.workStartDate} - {data.workEndDate}</p>
-            <p className="experience-position">{data.positionTitle}</p>
-            <p className="experience-description">{data.roleAchievements}</p>
+            <p className="experience-company">
+                {data.companyName && <span className="company-name">{data.companyName.toUpperCase()}</span>}
+                {data.companyName && data.workStartDate && <span>, </span>}
+                {data.workStartDate && <span className="work-start-date">{convertDate(data.workStartDate)} - </span>}
+                {data.workStartDate && <span className="work-end-date">{data.workEndDate ? convertDate(data.workEndDate) : 'PRESENT'}</span>}
+            </p>
+            {data.positionTitle && <p className="experience-position">{data.positionTitle}</p>}
+            {data.roleAchievements && <p className="experience-achievements">{data.roleAchievements}</p>}
         </div>
-    )
+    ) : null;
 };
 
-const EducationItem = (data) => {
-    return (
+const EducationItem = ({ data }) => {
+    return !dataIsEmpty(data) ? (
         <div className="education-item">
-            <p className="education-school">{data.schoolName}, {data.studyStartDate} - {data.workStartDate}</p>
-            <p className="education-degree">{data.studyTitle}: {data.studyScore}</p>
+            <p className="education-school">
+                {data.schoolName && <span className="school-name">{data.schoolName.toUpperCase()}</span>}
+                {data.schoolName && data.studyStartDate && <span>, </span>}
+                {data.studyStartDate &&<span className="study-start-date">{convertDate(data.studyStartDate)} - </span>}
+                {data.studyStartDate && <span className="study-end-date">{data.studyEndDate ? convertDate(data.studyEndDate) : 'PRESENT'}</span>}
+            </p>
+            <p className="education-degree">
+                {data.studyTitle && <span className="study-title">{data.studyTitle}</span>}
+                {data.studyTitle && data.studyScore && <span>: </span>}
+                {data.studyScore && <span className="study-score">{data.studyScore}</span>}
+            </p>
         </div>
-    )
+    ) : null;
 };
 
-const PreviewPage = (formData) => {
-    const data = formData.formData;
+const PreviewPage = ({ data }) => {
     console.log(data);
 
     return (
@@ -37,26 +69,26 @@ const PreviewPage = (formData) => {
                         <h4 className="subsection-title">CONTACT ME</h4>
                         <div className="contact-details-list">
                             <div className="contact-detail-item">
-                                <span className="contact-icon"></span>
+                                <span className="contact-icon"><img src={`${phoneIcon}`} /></span>
                                 <p>{data.personal[0].phoneNumber}</p>
                             </div>
                             <div className="contact-detail-item">
-                                <span className="contact-icon"></span>
+                                <span className="contact-icon"><img src={`${emailIcon}`} /></span>
                                 <p className="contact-text">{data.personal[0].email}</p>
                             </div>
                             <div className="contact-detail-item">
-                                <span className="contact-icon"></span>
+                                <span className="contact-icon"><img src={`${locationIcon}`} /></span>
                                 <p className="contact-text">{data.personal[0].location}</p>
                             </div>
                             <div className="contact-detail-item">
-                                <span className="contact-icon"></span>
+                                <span className="contact-icon"><img src={`${websiteIcon}`} /></span>
                                 <p className="contact-text">{data.personal[0].websiteLink}</p>
                             </div>
                         </div>
                     </span>
                     <span className="page-subsection">
                         <h4 className="subsection-title">ABOUT ME</h4>
-                        <p className="contact-text">{data.personal[0].professionalSummary}</p>
+                        <p className="about-me-text">{data.personal[0].professionalSummary}</p>
                     </span>
                 </div>
 
